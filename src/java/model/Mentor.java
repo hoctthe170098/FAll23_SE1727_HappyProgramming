2a7fe78fc9cb2f696a8094abb8089e1408dd51da
hoctthe170098 <hoctthe170098@fpt.edu.vn>
2023-09-18 15:42:25 +0700

    update mentor
    done

--- src/java/model/Mentor.java
+++ src/java/model/Mentor.java
@@ -10,12 +10,16 @@ package model;
  */
 public class Mentor {
    int ID;
+   float rate;
+   String experience;
 
     public Mentor() {
     }
 
-    public Mentor(int ID) {
+    public Mentor(int ID, float rate, String experience) {
         this.ID = ID;
+        this.rate = rate;
+        this.experience = experience;
     }
 
     public int getID() {
@@ -26,9 +30,27 @@ public class Mentor {
         this.ID = ID;
     }
 
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
     @Override
     public String toString() {
-        return "Mentor{" + "ID=" + ID + '}';
+        return "Mentor{" + "ID=" + ID + ", rate=" + rate + ", experience=" + experience + '}';
     }
+   
+    
    
 }
