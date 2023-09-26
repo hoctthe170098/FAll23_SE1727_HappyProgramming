c2d28249c13902f3272c5954da3402b1089aa56e
dungpmhe170403 <133070225+minhdung123@users.noreply.github.com>
2023-09-26 22:19:33 +0700

    forgot pass

--- src/java/controller/ProfileServlet.java
+++ src/java/controller/ProfileServlet.java
@@ -9,6 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Date;
 import model.*;
+
 @MultipartConfig
 public class ProfileServlet extends HttpServlet {
 
@@ -49,27 +50,26 @@ public class ProfileServlet extends HttpServlet {
             String fileName = Paths.get(part.getSubmittedFileName()).getFileName().toString();
             Account acc = (Account) request.getSession().getAttribute("acc");
             int ID = acc.getID();
-            int index=-1;
-            for (int i=0;i<=fileName.length();i++){
-                if(fileName.charAt(i)=='.'){
-                    index=i;break;
-                }
-            }
-            String s=fileName.substring(index);
-            //out.println(s);          
+            
+            String ss="fdf.df";
+            out.println(ss.contains("."));
+            String[] s = ss.split(".");
+            out.println(s.length);
             if (!Files.exists(Paths.get(realPath))) {
                 Files.createDirectory(Paths.get(realPath));
             }
-            String imgName="acc"+ID+s;
+            //String imgName=realPath+"/"+"acc"+ID+"."+str[str.length-1];
             //String imgName = realPath + "/" + fileName;
-            part.write(realPath+"/"+imgName);   
+            //part.write(imgName);
             
-            //out.print("<img src='imagesAcc/"+imgName+"'>");          
-            Profile p = new Profile(ID, gender,"imagesAcc/"+ imgName, Phone, birth, fullname, address, faceLink, intaLink);        
-            ProfileDAO dao = new ProfileDAO();
-            dao.InsertProfile(p);
-            //out.print(p.toString());         
-            request.getRequestDispatcher("profile.jsp").forward(request, response);
+//            out.print("<img src='imagesAcc/"+fileName+"'>");
+
+//            
+//            Profile p = new Profile(ID, gender, fileName, Phone, birth, fullname, address, faceLink, intaLink);        
+//            ProfileDAO dao = new ProfileDAO();
+//          dao.InsertProfile(p);
+//          out.print(p.toString());
+//          request.getRequestDispatcher("profile.jsp").forward(request, response);
         } catch (Exception e) {
 
         }
--- web/mentor.jsp
+++ web/mentor.jsp
@@ -15,6 +15,8 @@
     <head>
         <meta charset="utf-8">
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+<<<<<<< HEAD
         <title>Home</title>
         <meta content="" name="description">
         <meta content="" name="keywords">
@@ -26,6 +28,7 @@
         <!-- Favicons -->
         <link href="assets/img/favicon.png" rel="icon">
         <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+=======
   <title>Mentor</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
@@ -37,6 +40,8 @@
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+>>>>>>> 595401a21149daca8f99a050e91779e5905dff4e
+
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
